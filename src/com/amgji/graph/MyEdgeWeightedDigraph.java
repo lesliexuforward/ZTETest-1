fe72a71a57d42d9d8de218f4ec66031073cf9864
amgji <transamgjimtc@gmail.com>
2017-04-20 19:21:08 +0800

    update

--- src/com/amgji/graph/MyEdgeWeightedDigraph.java
+++ src/com/amgji/graph/MyEdgeWeightedDigraph.java
@@ -3,18 +3,15 @@ package com.amgji.graph;
 import java.io.File;
 
 import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.Edge;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class MyEdgeWeightedDigraph {
 	private static final String NEWLINE = System.getProperty("line.separator");
 
 	private final int V; // 顶点总数
 	private int E; // 边的总数
-	private Bag<Edge>[] adj; // 邻接表
+	private Bag<MyEdge>[] adj; // 邻接表
 
 	/**
 	 * 初始化一个顶点数为V边数为0的加权无向图
@@ -24,27 +21,27 @@ public class MyEdgeWeightedDigraph {
 			throw new IllegalArgumentException("Number of vertices must be nonnegative");
 		this.V = V;
 		this.E = 0;
-		adj = (Bag<Edge>[]) new Bag[V];
+		adj = (Bag<MyEdge>[]) new Bag[V];
 		for (int v = 0; v < V; v++) {
-			adj[v] = new Bag<Edge>();
+			adj[v] = new Bag<MyEdge>();
 		}
 	}
 
-	/**
-	 * 初始化一个顶点数为V边数为E的随机的加权无向图
-	 */
-	public MyEdgeWeightedDigraph(int V, int E) {
-		this(V);
-		if (E < 0)
-			throw new IllegalArgumentException("Number of edges must be nonnegative");
-		for (int i = 0; i < E; i++) {
-			int v = StdRandom.uniform(V);
-			int w = StdRandom.uniform(V);
-			double weight = Math.round(100 * StdRandom.uniform()) / 100.0;
-			Edge e = new Edge(v, w, weight);
-			addEdge(e);
-		}
-	}
+//	/**
+//	 * 初始化一个顶点数为V边数为E的随机的加权无向图
+//	 */
+//	public MyEdgeWeightedDigraph(int V, int E) {
+//		this(V);
+//		if (E < 0)
+//			throw new IllegalArgumentException("Number of MyEdges must be nonnegative");
+//		for (int i = 0; i < E; i++) {
+//			int v = StdRandom.uniform(V);
+//			int w = StdRandom.uniform(V);
+//			double weight = Math.round(100 * StdRandom.uniform()) / 100.0;
+//			MyEdge e = new MyEdge(v, w, weight);
+//			addMyEdge(e);
+//		}
+//	}
 
 	/**
 	 * 从流中读取一个加权无向图 如：src/resource/data.txt
@@ -53,35 +50,35 @@ public class MyEdgeWeightedDigraph {
 		this(in.readInt());
 		int E = in.readInt();
 		if (E < 0)
-			throw new IllegalArgumentException("Number of edges must be nonnegative");
+			throw new IllegalArgumentException("Number of MyEdges must be nonnegative");
 		for (int i = 0; i < E; i++) {
 			int v = in.readInt();
 			int w = in.readInt();
 			validateVertex(v);
 			validateVertex(w);
-			double weight = in.readDouble();
-			Edge e = new Edge(v, w, weight);
-			addEdge(e);
+			int weight = in.readInt();
+			MyEdge e = new MyEdge(v, w, weight);
+			addMyEdge(e);
 		}
 	}
 
-	/**
-	 * 深度复制一个加权无向图
-	 */
-	public MyEdgeWeightedDigraph(MyEdgeWeightedDigraph G) {
-		this(G.V());
-		this.E = G.E();
-		for (int v = 0; v < G.V(); v++) {
-			// reverse so that adjacency list is in same order as original
-			Stack<Edge> reverse = new Stack<Edge>();
-			for (Edge e : G.adj[v]) {
-				reverse.push(e);
-			}
-			for (Edge e : reverse) {
-				adj[v].add(e);
-			}
-		}
-	}
+//	/**
+//	 * 深度复制一个加权无向图
+//	 */
+//	public MyEdgeWeightedDigraph(MyEdgeWeightedDigraph G) {
+//		this(G.V());
+//		this.E = G.E();
+//		for (int v = 0; v < G.V(); v++) {
+//			// reverse so that adjacency list is in same order as original
+//			Stack<MyEdge> reverse = new Stack<MyEdge>();
+//			for (MyEdge e : G.adj[v]) {
+//				reverse.push(e);
+//			}
+//			for (MyEdge e : reverse) {
+//				adj[v].add(e);
+//			}
+//		}
+//	}
 
 	/**
 	 * 
@@ -108,7 +105,7 @@ public class MyEdgeWeightedDigraph {
 	/**
 	 * 为这个无向图添加一条边
 	 */
-	public void addEdge(Edge e) {
+	public void addMyEdge(MyEdge e) {
 		int v = e.either();
 		int w = e.other(v);
 		validateVertex(v);
@@ -123,7 +120,7 @@ public class MyEdgeWeightedDigraph {
 	 * @param v 顶点
 	 * @return 和v相关联的所有边
 	 */
-	public Iterable<Edge> adj(int v) {
+	public Iterable<MyEdge> adj(int v) {
 		validateVertex(v);
 		return adj[v];
 	}
@@ -141,11 +138,11 @@ public class MyEdgeWeightedDigraph {
 	/**
 	 * @return 图的所有边
 	 */
-	public Iterable<Edge> edges() {
-		Bag<Edge> list = new Bag<Edge>();
+	public Iterable<MyEdge> MyEdges() {
+		Bag<MyEdge> list = new Bag<MyEdge>();
 		for (int v = 0; v < V; v++) {
 			int selfLoops = 0;
-			for (Edge e : adj(v)) {
+			for (MyEdge e : adj(v)) {
 				if (e.other(v) > v) {
 					list.add(e);
 				}
@@ -166,7 +163,7 @@ public class MyEdgeWeightedDigraph {
 		s.append(V + " " + E + NEWLINE);
 		for (int v = 0; v < V; v++) {
 			s.append(v + ": ");
-			for (Edge e : adj[v]) {
+			for (MyEdge e : adj[v]) {
 				s.append(e + "  ");
 			}
 			s.append(NEWLINE);
