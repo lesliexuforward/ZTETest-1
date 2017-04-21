c47cfba8f5f046204d140b562b5b2b90e65ac1be
Bishop G <transamgjimtc@gmail.com>
2017-04-21 09:08:57 +0800

    update
    日常更新

--- src/com/amgji/graphbase/MyEdgeWeightedDigraph.java
+++ src/com/amgji/graphbase/MyEdgeWeightedDigraph.java
@@ -2,7 +2,6 @@ package com.amgji.graphbase;
 
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public class MyEdgeWeightedDigraph {
@@ -14,13 +13,7 @@ public class MyEdgeWeightedDigraph {
 	private int[] indegree; // indegree[v] = indegree of vertex v
 
 	/**
-	 * Initializes an empty edge-weighted digraph with {@code V} vertices and 0
-	 * edges.
-	 *
-	 * @param V
-	 *            the number of vertices
-	 * @throws IllegalArgumentException
-	 *             if {@code V < 0}
+	 * 初始化一个顶点数为V边数为0的加权有向图
 	 */
 	public MyEdgeWeightedDigraph(int V) {
 		if (V < 0)
@@ -34,17 +27,7 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Initializes an edge-weighted digraph from the specified input stream. The
-	 * format is the number of vertices <em>V</em>, followed by the number of
-	 * edges <em>E</em>, followed by <em>E</em> pairs of vertices and edge
-	 * weights, with each entry separated by whitespace.
-	 *
-	 * @param in
-	 *            the input stream
-	 * @throws IllegalArgumentException
-	 *             if the endpoints of any edge are not in prescribed range
-	 * @throws IllegalArgumentException
-	 *             if the number of vertices or edges is negative
+	 * 从流中读取一个加权有向图 如：src/resource/data.txt
 	 */
 	public MyEdgeWeightedDigraph(In in) {
 		this(in.readInt());
@@ -62,41 +45,16 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Initializes a new edge-weighted digraph that is a deep copy of {@code G}.
-	 *
-	 * @param G
-	 *            the edge-weighted digraph to copy
-	 */
-	public MyEdgeWeightedDigraph(MyEdgeWeightedDigraph G) {
-		this(G.V());
-		this.E = G.E();
-		for (int v = 0; v < G.V(); v++)
-			this.indegree[v] = G.indegree(v);
-		for (int v = 0; v < G.V(); v++) {
-			// reverse so that adjacency list is in same order as original
-			Stack<MyDirectedEdge> reverse = new Stack<MyDirectedEdge>();
-			for (MyDirectedEdge e : G.adj[v]) {
-				reverse.push(e);
-			}
-			for (MyDirectedEdge e : reverse) {
-				adj[v].add(e);
-			}
-		}
-	}
-
-	/**
-	 * Returns the number of vertices in this edge-weighted digraph.
-	 *
-	 * @return the number of vertices in this edge-weighted digraph
+	 * 
+	 * @return 返回这个无向图顶点的总数
 	 */
 	public int V() {
 		return V;
 	}
 
 	/**
-	 * Returns the number of edges in this edge-weighted digraph.
-	 *
-	 * @return the number of edges in this edge-weighted digraph
+	 * 
+	 * @return 返回这个无向图边的总数
 	 */
 	public int E() {
 		return E;
@@ -109,13 +67,7 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Adds the directed edge {@code e} to this edge-weighted digraph.
-	 *
-	 * @param e
-	 *            the edge
-	 * @throws IllegalArgumentException
-	 *             unless endpoints of edge are between {@code 0} and
-	 *             {@code V-1}
+	 * 为这个无向图添加一条边
 	 */
 	public void addEdge(MyDirectedEdge e) {
 		int v = e.from();
@@ -128,13 +80,10 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Returns the directed edges incident from vertex {@code v}.
-	 *
+	 * 
 	 * @param v
-	 *            the vertex
-	 * @return the directed edges incident from vertex {@code v} as an Iterable
-	 * @throws IllegalArgumentException
-	 *             unless {@code 0 <= v < V}
+	 *            顶点
+	 * @return 和v相关联的所有边
 	 */
 	public Iterable<MyDirectedEdge> adj(int v) {
 		validateVertex(v);
@@ -142,14 +91,9 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Returns the number of directed edges incident from vertex {@code v}. This
-	 * is known as the <em>outdegree</em> of vertex {@code v}.
-	 *
+	 * 
 	 * @param v
-	 *            the vertex
-	 * @return the outdegree of vertex {@code v}
-	 * @throws IllegalArgumentException
-	 *             unless {@code 0 <= v < V}
+	 * @return 返回从这个顶点出发的边的个数
 	 */
 	public int outdegree(int v) {
 		validateVertex(v);
@@ -157,14 +101,9 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Returns the number of directed edges incident to vertex {@code v}. This
-	 * is known as the <em>indegree</em> of vertex {@code v}.
-	 *
+	 * 
 	 * @param v
-	 *            the vertex
-	 * @return the indegree of vertex {@code v}
-	 * @throws IllegalArgumentException
-	 *             unless {@code 0 <= v < V}
+	 * @return 返回已v为终点的边的个数
 	 */
 	public int indegree(int v) {
 		validateVertex(v);
@@ -172,10 +111,6 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Returns all directed edges in this edge-weighted digraph. To iterate over
-	 * the edges in this edge-weighted digraph, use foreach notation:
-	 * {@code for (MyDirectedEdge e : G.edges())}.
-	 *
 	 * @return all edges in this edge-weighted digraph, as an iterable
 	 */
 	public Iterable<MyDirectedEdge> edges() {
@@ -188,13 +123,6 @@ public class MyEdgeWeightedDigraph {
 		return list;
 	}
 
-	/**
-	 * Returns a string representation of this edge-weighted digraph.
-	 *
-	 * @return the number of vertices <em>V</em>, followed by the number of
-	 *         edges <em>E</em>, followed by the <em>V</em> adjacency lists of
-	 *         edges
-	 */
 	public String toString() {
 		StringBuilder s = new StringBuilder();
 		s.append(V + " " + E + NEWLINE);
@@ -209,10 +137,9 @@ public class MyEdgeWeightedDigraph {
 	}
 
 	/**
-	 * Unit tests the {@code MyEdgeWeightedDigraph} data type.
-	 *
+	 * 单元测试
+	 * 
 	 * @param args
-	 *            the command-line arguments
 	 */
 	public static void main(String[] args) {
 		In in = new In(args[0]);
